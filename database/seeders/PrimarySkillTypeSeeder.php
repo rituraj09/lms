f18422d18ae8bb4b31be6b526861c064c721c615
Rituraj Borgohain <riturajborgohain@gmail.com>
2026-05-27 17:27:07 +0530

    do ne

---
+++ database/seeders/PrimarySkillTypeSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\PrimarySkillType;
+
+class PrimarySkillTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $skills = [
+            [
+                'name' => 'Cognitive Skill',
+                'slug' => 'cognitive',
+                'description' => 'Skills related to mental processes.',
+            ],
+
+            [
+                'name' => 'Life Skill',
+                'slug' => 'life',
+                'description' => 'Interpersonal and intrapersonal skills.',
+            ],
+
+            [
+                'name' => 'Leadership Skill',
+                'slug' => 'leadership',
+                'description' => 'Skills related to leadership and influence.',
+            ],
+        ];
+
+        foreach ($skills as $skill) {
+            PrimarySkillType::updateOrCreate(
+                ['slug' => $skill['slug']],
+                $skill
+            );
+        }
+    }
+}
